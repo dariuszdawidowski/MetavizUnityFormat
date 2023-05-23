49f981e6fccb0d7e2181373e5ebe229d6852cd91
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-05-23 23:50:19 +0200

    Changed gitignore. Bare Class fo plugin.

---
+++ MetavizUnityFormat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetavizUnityFormat
+{
+
+    public MetavizUnityFormat()
+    {
+
+    }
+
+    public void parse(string buffer)
+    {
+        Debug.Log("parsing");
+        Debug.Log(buffer);
+    }
+
+}
